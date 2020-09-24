87432bd7ee6f05e8421ed7377fd895849bd2356d
Christopher Montesano <c.e.montesano@gmail.com>
2020-09-23 20:13:42 -0400

    support for argparse choices

--- xappt/models/parameter/convert.py
+++ xappt/models/parameter/convert.py
@@ -7,14 +7,13 @@ def to_argument_dict(parameter_dict: Dict) -> Tuple[List, Dict]:
     if short_name is not None:
         positional_args.append(f"-{short_name}")
 
-    # TODO: add support for choices
-
     keyword_args = {}
     if parameter_dict['data_type'] == bool:
         keyword_args['action'] = 'store_true'
     else:
         keyword_args['action'] = 'store'
         keyword_args['type'] = parameter_dict['data_type']
+        keyword_args['choices'] = parameter_dict.get('choices')
 
     keyword_args['help'] = parameter_dict['description']
 
