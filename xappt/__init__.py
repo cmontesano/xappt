c81a59911cc6bcb5351c2dc6c5b197020f2de7a7
Christopher Montesano <c.e.montesano@gmail.com>
2020-09-25 19:16:05 -0400

    expose Parameter, call discovery on init

--- xappt/__init__.py
+++ xappt/__init__.py
@@ -6,6 +6,7 @@ from xappt.managers import plugin_manager
 from xappt.managers.plugin_manager import *
 
 from xappt.models import BaseTool, BaseInterface
+from xappt.models.parameter.model import Parameter
 from xappt.models.parameter.parameters import *
 from xappt.models.parameter.errors import *
 from xappt.models.parameter.validators import *
@@ -14,6 +15,7 @@ from xappt.utilities import *
 
 from xappt.plugins import *
 
+xappt.discover_plugins()
 
 version = tuple(map(int, xappt.__version__.__version__.split('.'))) + (xappt.__version__.__build__, )
 version_str = f"{xappt.__version__.__version__}-{xappt.__version__.__build__}"
