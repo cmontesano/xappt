bc741d207c439d601f8f569df993f8faaf276162
Christopher Montesano <c.e.montesano@gmail.com>
2020-09-26 15:29:45 -0400

    refine cli

--- xappt/cli.py
+++ xappt/cli.py
@@ -1,54 +1,61 @@
 import argparse
+import os
 import sys
-
-from typing import Generator
 
 import xappt
 
 from xappt.models.parameter import convert
 
 
-def cli_main(*argv) -> int:
+def build_parser() -> argparse.ArgumentParser:
     interface_list = [i[0] for i in xappt.plugin_manager.registered_interfaces()]
-    interface_list.insert(0, "none")
+    default_interface_name = os.environ.get(xappt.INTERFACE_ENV, xappt.INTERFACE_DEFAULT)
 
     parser = argparse.ArgumentParser()
 
-    parser.add_argument('--version', action='store_true', dest='version', help='Display the version number')
-    parser.add_argument('--interface', choices=interface_list, default="none")
+    parser.add_argument('-v', '--version', action='store_true',
+                        help='Display the version number and build')
+    parser.add_argument('-i', '--interface', choices=interface_list, default=default_interface_name,
+                        help='Specify the name of the default user interface. '
+                             f'This can also be done by setting the environment variable {xappt.INTERFACE_ENV}')
+    parser.add_argument('--ui', action='store_true', dest="interactive",
+                        help='Run the specified tool interactively using the passed command line arguments, '
+                             'otherwise it will be invoked with the default interface.')
 
     subparsers = parser.add_subparsers(help="Sub command help", dest='command')
 
     for plugin_name, plugin_class in xappt.plugin_manager.registered_tools():
-        assert issubclass(plugin_class, xappt.BaseTool)
         plugin_parser = subparsers.add_parser(plugin_class.name().lower(), help=plugin_class.help())
         for parameter in plugin_class.class_parameters():
             setup_args = parameter.param_setup_args
             args, kwargs = convert.to_argument_dict(setup_args)
             plugin_parser.add_argument(*args, **kwargs)
 
+    return parser
+
+
+def cli_main(*argv) -> int:
+    parser = build_parser()
     options = parser.parse_args(args=argv)
 
     if options.version:
         print(f"xappt {xappt.version_str}")
         return 0
 
-    if options.command is not None:
-        if options.interface == "none":
-            interface = None
-        else:
-            interface = xappt.plugin_manager.get_interface_plugin(options.interface)()
+    os.environ.setdefault(xappt.INTERFACE_ENV, options.interface)
+    interface_class = xappt.plugin_manager.get_default_interface()
+    interface = interface_class()
 
-        try:
-            plugin_class = xappt.plugin_manager.get_tool_plugin(options.command)
-        except ValueError:
-            print(f"Plugin {options.command} not found")
+    if options.command is not None:
+        plugin_class = xappt.plugin_manager.get_tool_plugin(options.command)
+        plugin_instance = plugin_class(**options.__dict__)
+        if options.interactive:
+            interface.invoke(plugin_instance)
         else:
-            plugin_instance = plugin_class(**options.__dict__)
             plugin_instance.validate()
             return plugin_instance.execute(interface=interface)
-
-    parser.print_help()
+    else:
+        parser.print_help()
 
     return 0
 
