a317197902b5494b658be775a8b308d5dd415422
Christopher Montesano <c.e.montesano@gmail.com>
2020-09-30 21:23:33 -0400

    deferred callback manipulation

--- xappt/models/parameter/model.py
+++ xappt/models/parameter/model.py
@@ -2,7 +2,9 @@ from __future__ import annotations
 
 import copy
 
-from typing import Any, Callable, Dict, List, Optional, Sequence, Set, Tuple, Type
+from collections import defaultdict
+
+from typing import Any, Callable, Dict, DefaultDict, List, Optional, Sequence, Set, Tuple, Type
 from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
@@ -12,20 +14,32 @@ if TYPE_CHECKING:
 class ParameterCallback:
     def __init__(self):
         self._callback_functions: Set[Callable] = set()
+        self._deferred_operations: DefaultDict[str, Set[Optional[Callable]]] = defaultdict(set)
 
     def add(self, cb: Callable):
-        self._callback_functions.add(cb)
+        self._deferred_operations['add'].add(cb)
 
     def remove(self, cb: Callable):
-        try:
-            self._callback_functions.remove(cb)
-        except KeyError:
-            pass
+        self._deferred_operations['remove'].add(cb)
 
     def clear(self):
-        self._callback_functions.clear()
+        self._deferred_operations['clear'].add(None)
+
+    def _run_deferred_ops(self):
+        for operation, functions in self._deferred_operations.items():
+            op_fn = getattr(self._callback_functions, operation)
+            for function in functions:
+                if function is None:
+                    op_fn()
+                else:
+                    try:
+                        op_fn(function)
+                    except KeyError:
+                        pass
+        self._deferred_operations.clear()
 
     def invoke(self):
+        self._run_deferred_ops()
         for fn in self._callback_functions:
             fn()
 
