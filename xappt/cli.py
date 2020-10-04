8f5ea20d0041e69bb6c4da1bc94bd0d579c943cd
Christopher Montesano <c.e.montesano@gmail.com>
2020-10-04 08:24:00 -0400

    add flag to list all plugins
    Note that this only lists those plugins which have been registered at startup

--- xappt/cli.py
+++ xappt/cli.py
@@ -5,6 +5,13 @@ import logging
 import os
 import sys
 
+from collections import defaultdict
+from itertools import chain
+from typing import DefaultDict, List
+
+import colorama
+from colorama import Fore
+
 import xappt
 
 from xappt.models.parameter import convert
@@ -19,7 +26,9 @@ def build_parser() -> argparse.ArgumentParser:
     parser.add_argument('-v', '--version', action='store_true',
                         help='Display the version number and build')
     parser.add_argument('-d', '--debug', action='store_true',
-                        help='Set the logging level to DEBUG and ensure a StreamHandler')
+                        help='Set the logging level to DEBUG and create a StreamHandler')
+    parser.add_argument('-l', '--list', action='store_true',
+                        help='List all of the discovered plugins')
     parser.add_argument('-i', '--interface', choices=interface_list, default=default_interface_name,
                         help='Specify the name of the default user interface. '
                              f'This can also be done by setting the environment variable {xappt.INTERFACE_ENV}')
@@ -39,6 +48,21 @@ def build_parser() -> argparse.ArgumentParser:
     return parser
 
 
+def list_all_plugins():
+    colorama.init(autoreset=True)
+
+    plugin_list: DefaultDict[str, List[str]] = defaultdict(list)
+
+    for _, plugin_class in chain(xappt.plugin_manager.registered_interfaces(), xappt.plugin_manager.registered_tools()):
+        collection = plugin_class.collection()
+        plugin_list[collection].append(f"{Fore.LIGHTBLUE_EX}{plugin_class.name()} {Fore.WHITE}({plugin_class.help() or 'No help text'})")
+
+    for collection in sorted(plugin_list.keys()):
+        print(f"{Fore.GREEN}{collection}")
+        for plugin in sorted(plugin_list[collection]):
+            print(f"    {plugin}")
+
+
 def cli_main(*argv) -> int:
     parser = build_parser()
     options = parser.parse_args(args=argv)
@@ -50,6 +74,10 @@ def cli_main(*argv) -> int:
     if options.debug:
         logger = logging.getLogger(xappt.APP_NAME)
         logger.addHandler(logging.StreamHandler())
+
+    if options.list:
+        list_all_plugins()
+        return 0
 
     os.environ[xappt.INTERFACE_ENV] = options.interface
 
