c146772df0ea710532a1cf9a63bf9a31b828f722
Christopher Montesano <c.e.montesano@gmail.com>
2020-10-04 09:33:55 -0400

    move env var name to constants

--- xappt/config.py
+++ xappt/config.py
@@ -1,7 +1,7 @@
 import logging
 import os
 
-from xappt.constants import APP_NAME
+from xappt.constants import APP_NAME, DEBUG_FLAG_ENV
 
 log = logging.getLogger(APP_NAME)
 sh = logging.StreamHandler()
@@ -10,5 +10,5 @@ formatter = logging.Formatter("[%(levelname)8s] %(name)s : %(message)s")
 sh.setFormatter(formatter)
 log.addHandler(sh)
 
-if os.environ.get("XAPPT_DEBUG", "0") != "0":
+if os.environ.get(DEBUG_FLAG_ENV, "0") != "0":
     log.setLevel(logging.DEBUG)
