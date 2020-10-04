312168deb87c9cebbed2236a8d162f8ff8081e46
Christopher Montesano <c.e.montesano@gmail.com>
2020-10-04 09:45:54 -0400

    fix default log level

--- xappt/config.py
+++ xappt/config.py
@@ -1,14 +1,16 @@
 import logging
 import os
 
-from xappt.constants import APP_NAME, DEBUG_FLAG_ENV
+from xappt.constants import APP_NAME
 
 log = logging.getLogger(APP_NAME)
+log.setLevel(logging.WARN)
 sh = logging.StreamHandler()
-sh.setLevel(logging.DEBUG)
 formatter = logging.Formatter("[%(levelname)8s] %(name)s : %(message)s")
 sh.setFormatter(formatter)
+sh.setLevel(logging.WARN)
 log.addHandler(sh)
 
-if os.environ.get(DEBUG_FLAG_ENV, "0") != "0":
+if os.environ.get("XAPPT_DEBUG", "0") != "0":
+    sh.setLevel(logging.DEBUG)
     log.setLevel(logging.DEBUG)
