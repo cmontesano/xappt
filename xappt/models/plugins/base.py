a4741a3c3ddbaf8a26cdaba404daa58090eccb5d
Christopher Montesano <c.e.montesano@gmail.com>
2020-10-04 08:11:50 -0400

    support collections in plugin definition

--- xappt/models/plugins/base.py
+++ xappt/models/plugins/base.py
@@ -1,8 +1,15 @@
 class BasePlugin:
     @classmethod
     def name(cls) -> str:
+        """ Use this to specify the name of your plugin. """
         return cls.__name__.lower()
 
     @classmethod
     def help(cls) -> str:
+        """ Provide a short help string or description. """
         return ""
+
+    @classmethod
+    def collection(cls) -> str:
+        """ Use this to group your plugin with other like plugins. """
+        return "other"
