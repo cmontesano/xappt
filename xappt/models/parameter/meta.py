9d4f3144ac8a2be174dbe8566e1276069340c7ab
Christopher Montesano <c.e.montesano@gmail.com>
2021-08-23 11:35:19 -0400

    load parameter names from `bases`

--- xappt/models/parameter/meta.py
+++ xappt/models/parameter/meta.py
@@ -4,7 +4,13 @@ from .model import ParameterDescriptor
 class ParamMeta(type):
     def __new__(mcs, name, bases, attrs):
         cls = type.__new__(mcs, name, bases, attrs)
-        cls._parameters_ = []
+        base_parameters = []
+        for base in bases:
+            try:
+                base_parameters += base._parameters_  # noqa
+            except AttributeError:
+                pass
+        cls._parameters_ = base_parameters
         for var_name, var_value in vars(cls).items():
             if isinstance(var_value, ParameterDescriptor):
                 cls._parameters_.append(var_name)
