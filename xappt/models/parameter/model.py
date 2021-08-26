81e7e3745280fba60e04a7c72d99e316fc48f31a
Christopher Montesano <c.e.montesano@gmail.com>
2021-08-26 13:28:04 -0400

    remove unused class `ParameterCallback`

--- xappt/models/parameter/model.py
+++ xappt/models/parameter/model.py
@@ -24,50 +24,6 @@ class ParamSetupDict(dict):
         super(ParamSetupDict, self).__init__(**kwargs)
 
 
-class ParameterCallback:
-    def __init__(self):
-        self._callback_functions: Set[Callable] = set()
-        self._deferred_operations: DefaultDict[str, Set[Optional[Callable]]] = defaultdict(set)
-        self._paused = False
-
-    def add(self, cb: Callable):
-        self._deferred_operations['add'].add(cb)
-
-    def remove(self, cb: Callable):
-        self._deferred_operations['remove'].add(cb)
-
-    def clear(self):
-        self._deferred_operations['clear'].add(None)
-
-    def _run_deferred_ops(self):
-        for operation, functions in self._deferred_operations.items():
-            op_fn = getattr(self._callback_functions, operation)
-            for function in functions:
-                if function is None:
-                    op_fn()
-                else:
-                    try:
-                        op_fn(function)
-                    except KeyError:
-                        pass
-        self._deferred_operations.clear()
-
-    def invoke(self, sender: Parameter):
-        self._run_deferred_ops()
-        if self._paused:
-            return
-        for fn in self._callback_functions:
-            fn(param=sender)
-
-    @property
-    def paused(self) -> bool:
-        return self._paused
-
-    @paused.setter
-    def paused(self, value: bool):
-        self._paused = value
-
-
 class Parameter:
     def __init__(self, name: str, *, data_type: Type, **kwargs):
         self.name: str = name
