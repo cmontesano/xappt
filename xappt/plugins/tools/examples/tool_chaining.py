ffc945abbc4e4e171c6fed8652a9a086878271d7
Christopher Montesano <c.e.montesano@gmail.com>
2021-09-04 18:07:34 -0400

    tool_data example

--- xappt/plugins/tools/examples/tool_chaining.py
+++ xappt/plugins/tools/examples/tool_chaining.py
@@ -1,8 +1,8 @@
 import xappt
 
 TOOL_CHOICES = {
-    "data types": xappt.plugin_manager.get_tool_plugin("datatypes"),
-    "modify choices": xappt.plugin_manager.get_tool_plugin("modifychoices"),
+    "data types": "datatypes",
+    "modify choices": "modifychoices",
     "none (quit)": None,
 }
 
@@ -10,6 +10,7 @@ TOOL_CHOICES = {
 @xappt.register_plugin
 class ToolChaining(xappt.BaseTool):
     next_tool = xappt.ParamString(description="Choose the next task", choices=list(TOOL_CHOICES.keys()))
+    persistent_data = xappt.ParamString(description="This value should persist between tools")
 
     @classmethod
     def name(cls) -> str:
@@ -24,8 +25,18 @@ class ToolChaining(xappt.BaseTool):
         return "Examples"
 
     def execute(self, *, interface: xappt.BaseInterface, **kwargs) -> int:
-        next_tool_class = TOOL_CHOICES[self.next_tool.value]
-        if next_tool_class is not None:
-            interface.add_tool(next_tool_class)
-            interface.add_tool(ToolChaining)  # return here after the other tool
+        interface.tool_data['persistent_data'] = self.persistent_data.value
+
+        first_run = kwargs.get('first_run', True)
+        if first_run:
+            interface.write_stdout("Any queued tools will be initialized with the contents of `interface.tool_data`.")
+            interface.write_stdout("`interface.tool_data` will also be passed to `BaseTool.execute` as **kwargs.")
+            interface.write_stdout("This can be used not only to initialize subsequent")
+            interface.tool_data['first_run'] = False
+
+        next_tool_name = TOOL_CHOICES[self.next_tool.value]
+        if next_tool_name is not None:
+            interface.add_tool(next_tool_name)
+            interface.add_tool(ToolChaining)  # return here after the other tool runs
+
         return 0
