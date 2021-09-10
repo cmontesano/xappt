cfd66f2670d856332d7ee62b02e593e112b979b5
Christopher Montesano <c.e.montesano@gmail.com>
2021-09-09 20:24:31 -0400

    callback tests

--- tests/models/test_callback.py
+++ tests/models/test_callback.py
@@ -29,7 +29,7 @@ class TestCallback(unittest.TestCase):
         del cb_fn
         self.assertEqual(0, len(cb._callback_functions))
 
-    def test_remove(self):
+    def test_weakref1(self):
         cb_fn = CallbackFunction()
         cb = Callback()
         cb.add(cb_fn)
@@ -38,3 +38,43 @@ class TestCallback(unittest.TestCase):
         cb.remove(cb_fn)
         cb._run_deferred_ops()
         self.assertEqual(0, len(cb._callback_functions))
+
+    def test_weakref2(self):
+        cb = Callback()
+        cb.add(CallbackFunction())
+        cb._run_deferred_ops()
+        self.assertEqual(0, len(cb._callback_functions))
+
+    def test_clear(self):
+        cb_fn_1 = CallbackFunction()
+        cb_fn_2 = CallbackFunction()
+        cb_fn_3 = CallbackFunction()
+        cb = Callback()
+        cb.add(cb_fn_1)
+        cb.add(cb_fn_2)
+        cb.add(cb_fn_3)
+        cb._run_deferred_ops()
+        self.assertEqual(3, len(cb._callback_functions))
+        cb.clear()
+        cb._run_deferred_ops()
+        self.assertEqual(0, len(cb._callback_functions))
+
+    def test_invoke(self):
+        cb_fn = CallbackFunction()
+        cb = Callback()
+        cb.add(cb_fn)
+        cb.invoke("arg1", "arg2")
+        self.assertEqual(1, len(cb_fn.call_info))
+        self.assertIn("arg1", cb_fn.call_info[0][0])
+        self.assertIn("arg2", cb_fn.call_info[0][0])
+
+    def test_invoke_paused(self):
+        cb_fn = CallbackFunction()
+        cb = Callback()
+        cb.add(cb_fn)
+        self.assertFalse(cb.paused)
+        cb.paused = True
+        self.assertTrue(cb.paused)
+        cb.invoke("arg1", "arg2")
+        self.assertEqual(1, len(cb._callback_functions))
+        self.assertEqual(0, len(cb_fn.call_info))
