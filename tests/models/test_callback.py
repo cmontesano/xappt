22696fe096dfd26ef339c9802f3a3e73967b80f6
Christopher Montesano <c.e.montesano@gmail.com>
2021-09-10 21:14:28 -0400

    better callback tests

--- tests/models/test_callback.py
+++ tests/models/test_callback.py
@@ -3,51 +3,59 @@ import unittest
 from xappt.models.callback import Callback
 
 
-class CallbackFunction:
+class CallbackHost:
     def __init__(self):
-        self.call_info = []
+        self.call_info = {
+            'a': [],
+            'b': [],
+            'c': [],
+        }
 
-    def __call__(self, *args, **kwargs):
-        self.call_info.append((args, kwargs))
+    def callback_method_a(self, *args, **kwargs):
+        self.call_info['a'].append((args, kwargs))
+
+    def callback_method_b(self, *args, **kwargs):
+        self.call_info['b'].append((args, kwargs))
+
+    def callback_method_c(self, *args, **kwargs):
+        self.call_info['c'].append((args, kwargs))
 
 
 class TestCallback(unittest.TestCase):
     def test_add(self):
-        cb_fn = CallbackFunction()
+        cb_host = CallbackHost()
         cb = Callback()
-        cb.add(cb_fn)
+        cb.add(cb_host.callback_method_a)
         self.assertEqual(0, len(cb._callback_functions))
         cb._run_deferred_ops()
         self.assertEqual(1, len(cb._callback_functions))
 
     def test_add_auto_remove(self):
-        cb_fn = CallbackFunction()
+        cb_host = CallbackHost()
         cb = Callback()
-        cb.add(cb_fn)
+        cb.add(cb_host.callback_method_a)
         cb._run_deferred_ops()
         self.assertEqual(1, len(cb._callback_functions))
-        del cb_fn
+        del cb_host
         cb._run_deferred_ops()
         self.assertEqual(0, len(cb._callback_functions))
 
     def test_weakref(self):
-        cb_fn = CallbackFunction()
+        cb_host = CallbackHost()
         cb = Callback()
-        cb.add(cb_fn)
+        cb.add(cb_host.callback_method_a)
         cb._run_deferred_ops()
         self.assertEqual(1, len(cb._callback_functions))
-        cb.remove(cb_fn)
+        cb.remove(cb_host.callback_method_a)
         cb._run_deferred_ops()
         self.assertEqual(0, len(cb._callback_functions))
 
     def test_clear(self):
-        cb_fn_1 = CallbackFunction()
-        cb_fn_2 = CallbackFunction()
-        cb_fn_3 = CallbackFunction()
+        cb_host = CallbackHost()
         cb = Callback()
-        cb.add(cb_fn_1)
-        cb.add(cb_fn_2)
-        cb.add(cb_fn_3)
+        cb.add(cb_host.callback_method_a)
+        cb.add(cb_host.callback_method_b)
+        cb.add(cb_host.callback_method_c)
         cb._run_deferred_ops()
         self.assertEqual(3, len(cb._callback_functions))
         cb.clear()
@@ -55,21 +63,22 @@ class TestCallback(unittest.TestCase):
         self.assertEqual(0, len(cb._callback_functions))
 
     def test_invoke(self):
-        cb_fn = CallbackFunction()
+        cb_host = CallbackHost()
         cb = Callback()
-        cb.add(cb_fn)
+        cb.add(cb_host.callback_method_a)
         cb.invoke("arg1", "arg2")
-        self.assertEqual(1, len(cb_fn.call_info))
-        self.assertIn("arg1", cb_fn.call_info[0][0])
-        self.assertIn("arg2", cb_fn.call_info[0][0])
+        call_info = cb_host.call_info['a']
+        self.assertEqual(1, len(call_info))
+        self.assertIn("arg1", call_info[0][0])
+        self.assertIn("arg2", call_info[0][0])
 
     def test_invoke_paused(self):
-        cb_fn = CallbackFunction()
+        cb_host = CallbackHost()
         cb = Callback()
-        cb.add(cb_fn)
+        cb.add(cb_host.callback_method_a)
         self.assertFalse(cb.paused)
         cb.paused = True
         self.assertTrue(cb.paused)
         cb.invoke("arg1", "arg2")
         self.assertEqual(1, len(cb._callback_functions))
-        self.assertEqual(0, len(cb_fn.call_info))
+        self.assertEqual(0, len(cb_host.call_info['a']))
